a2e8cdc87385a860ad399808b5df69168bbe95ec
Jonathan Mendes <jonathan_mendes96@outlook.com>
2019-04-18 16:15:49 -0300

    Game Pedra, Papel e Tesoura

---
+++ js/index.js
@@ -0,0 +1,96 @@
+start()
+
+function start(){
+    clear()
+    validateButtons()
+    // setTimeout(start, 4000)
+}
+
+function validateButtons(){
+    var btnPedra = document.getElementById('btnPedra')
+    var btnPapel = document.getElementById('btnPapel')
+    var btnTesoura = document.getElementById('btnTesoura')
+    var btnJogar = document.getElementById('btnJogar')
+    var imgPlayer = document.getElementById('player')
+    var imgComp = document.getElementById('comp')
+    var end = "../img/"
+    var value = -1
+
+    btnPedra.onclick = function(){
+        imgPlayer.src = end + "pedra.png"
+        value = 0
+    }
+
+    btnPapel.onclick = function(){
+        imgPlayer.src = end + "papel.png"
+        value = 1
+    }
+
+    btnTesoura.onclick = function(){
+        imgPlayer.src = end + "tesoura.png"
+        value = 2
+    }
+
+    btnJogar.onclick = function(){
+        var rand = Math.floor(Math.random() * (3 - 0))
+
+        if(rand === 0){
+            imgComp.src = end + "pedra.png"
+        }else if(rand === 1){
+            imgComp.src = end + "papel.png"
+        }else{
+            imgComp.src = end + "tesoura.png"
+        }
+
+        result(value, rand)
+        setTimeout(start, 4000)
+    }
+    
+}
+
+function result(value, rand){
+    if(value === rand){
+        empateGame()
+    }else if((value === 0) && (rand === 2)){
+        playerWin()
+    }else if((value === 1) && (rand === 0)){
+        playerWin()
+    }else if((value === 2) && (rand === 1)){
+        playerWin()
+    }else if((rand === 0) && (value === 2)){
+        compWin()
+    }else if((rand === 1) && (value === 0)){
+        compWin()
+    }else if((rand === 2) && (value === 1)){
+        compWin()
+    }
+}
+
+function playerWin(){
+    var result = returnResult()
+    result.innerHTML = "Jogador 1 ganhou"
+}
+
+function compWin(){
+    var result = returnResult()
+    result.innerHTML = "Computador ganhou"
+}
+
+function empateGame(){
+    var result = returnResult()
+    result.innerHTML = "Empate"
+}
+
+function returnResult(){
+    return document.getElementById('result')
+}
+
+function clear(){
+    var imgPlayer = document.getElementById('player')
+    var imgComp = document.getElementById('comp')
+    var result = returnResult()
+
+    result.innerHTML = ""
+    imgPlayer.src = "../img/neutro.png"
+    imgComp.src = "../img/neutro.png"
+}
